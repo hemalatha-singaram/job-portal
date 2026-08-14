0ce0319ce952dd3a8f979f363115959a7cca99c7
hemalatha-singaram <hemalathanabi@gmail.com>
2026-08-14 14:30:42 +0530

    Secure jobs with recruiter ownership

--- recruiter/models.py
+++ recruiter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Job(models.Model):
@@ -10,25 +11,22 @@ class Job(models.Model):
         ('Remote', 'Remote'),
     ]
 
-    title = models.CharField(max_length=200)
-
-    company = models.CharField(max_length=200)
-
-    location = models.CharField(max_length=100)
-
-    salary = models.CharField(max_length=50)
-
-    job_type = models.CharField(
-        max_length=20,
-        choices=JOB_TYPES
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='posted_jobs'
     )
 
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    salary = models.CharField(max_length=50)
+    job_type = models.CharField(max_length=20, choices=JOB_TYPES)
     experience = models.CharField(max_length=50)
-
     skills = models.TextField()
-
     description = models.TextField()
-
     posted_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
