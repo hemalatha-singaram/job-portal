91e9c64c5193f3cfd30e6385e1260a547569faef
Hemalatha Singaram <hemalathanabi@gmail.com>
2026-09-11 20:26:29 +0530

    Clean candidate application deletion handler

--- candidate/application_actions.py
+++ candidate/application_actions.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
-from django.views.decorators.http import require_POST
 
 from .models import CandidateProfile, JobApplication
 
